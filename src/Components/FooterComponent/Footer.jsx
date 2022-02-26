161a6f8ff291eec934f913ceb8cd84056ef24800
Zachary Fisher <zacharyfisher808@gmail.com>
2022-02-25 16:04:59 -1000

    Update

--- src/Components/FooterComponent/Footer.jsx
+++ src/Components/FooterComponent/Footer.jsx
@@ -6,7 +6,7 @@ const Footer = () => {
     return(
         <Container fluid id="Links" className="footerContainer">
             <Row className="text-center position-relative top-50">
-                <Col xs={12}><h1>Some of my links</h1></Col>
+                <Col xs={12}><h1>Contact Me</h1></Col>
                 <Col><a href="https://github.com/Zryer" target="blank" className="footerGithub"><IconGITHUB /></a></Col>
                 <Col><a href="https://www.linkedin.com/in/zachary-fisher-1b6448221/" target="blank" className="footerLinkedin"><IconLINKEDIN /></a></Col>
             </Row>
